0c13a3f6772b1d1c187ee8b2da51bf4c8885b0e6
Terry Tang <917755100@qq.com>
2026-02-11 15:33:22 +0800

    Change theme from 'bento' to 'receipt'

--- moire.config.ts
+++ moire.config.ts
@@ -1,7 +1,7 @@
 export const config = {
   title: "Terry Story",
   author: "Terry",
-  theme: "bento",
+  theme: "receipt",
   pageSize: 20,
   utcOffset: 8,
   description: "Sync your thoughts from Apple Notes by Shortcuts.",
